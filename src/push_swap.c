3d1812025a9c741387f2ebc47ed6a1c2b7397a07
Damian Dania <damiandania@gmail.com>
2023-07-11 20:02:26 +0200

    comenzando

--- src/push_swap.c
+++ src/push_swap.c
@@ -1,7 +1,13 @@
 #include "../includes/push_swap.h"
 
-int main()
+int main(int ac, char **av)
 {
-	/* code */
+	t_stack	*stack_a;
+	t_stack	*stack_b;
+	
+	if (ac < 2)
+		return (1);
+	if (ac <= 3)
+
 	return 0;
 }
